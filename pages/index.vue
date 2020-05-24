bb2e75f1a5c33971b8a9b6b8738525ce76ded9ba
Kosuke Takahashi <yurufuwagangxxx@gmail.com>
2020-05-24 23:17:25 +0900

    [update]v-bindからpropsの値をとれるように修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="page">
-    <Component message="Hello Vue.js!"></Component>
+    <Component
+      :message="myMessage"
+    >
+    </Component>
   </div>
 </template>
 
 <script>
 import Component from '~/components/Component'
 export default {
+  data() {
+    return {
+      myMessage: 'v-bindを使ったprops'
+    }
+  },
   components: {
     Component: Component,
   }
